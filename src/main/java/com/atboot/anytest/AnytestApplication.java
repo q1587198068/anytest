bed07f62d60f855d46e77a2ff28ac9da83c0dd17
86185 <1587198068@qq.com>
2022-11-10 08:54:32 +0800

    Initial commit

---
+++ src/main/java/com/atboot/anytest/AnytestApplication.java
@@ -0,0 +1,13 @@
+package com.atboot.anytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnytestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atboot/anytest/AnytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atboot.anytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
